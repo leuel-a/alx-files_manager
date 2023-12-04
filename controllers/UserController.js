61a8d5cce182bbefec87b2bee12318d14febab49
nefelibatala1993 <leuela1993@gmail.com>
2023-12-04 20:18:46 +0300

    Fix duplicate import and add validation for email and password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,5 @@
 import dbClient from '../utils/db';
+import dbClient from '../utils/db';
 
 const postNew = async (req, res) => {
   const { email, password } = req.body;
@@ -18,5 +19,22 @@ const postNew = async (req, res) => {
   return res.json({ id: insertedId, email });
 };
 
-// eslint-disable-next-line import/prefer-default-export
-export { postNew };
+const postNew = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' });
+  }
+  if (!password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
+  const alreadyEmail = await dbClient.getUserByEmail(email);
+
+  if (alreadyEmail) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
+  const insertedId = await dbClient.insertOne(email, password);
+  return res.json({ id: insertedId, email });
+};
+
+export default postNew;
