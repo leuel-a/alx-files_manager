c9ba63185686f97efa482c4434ef5932a150a822
nefelibatala1993 <leuela1993@gmail.com>
2023-12-04 23:36:25 +0300

    Fix import statement in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UserController from '../controllers/UserController';
+import UserController from '../controllers/UsersController';
 
 const router = Router();
 
