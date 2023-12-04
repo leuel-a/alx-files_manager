57134ced335c2930341ce40030ad03f3f4f9cd49
nefelibatala1993 <leuela1993@gmail.com>
2023-12-04 22:11:02 +0300

    Refactor the UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,4 @@
 import dbClient from '../utils/db';
-import dbClient from '../utils/db';
-
-const postNew = async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email) {
-    return res.status(400).json({ error: 'Missing email' });
-  }
-  if (!password) {
-    return res.status(400).json({ error: 'Missing password' });
-  }
-  const alreadyEmail = await dbClient.getUserByEmail(email);
-
-  if (alreadyEmail) {
-    return res.status(400).json({ error: 'Already exist' });
-  }
-  const insertedId = await dbClient.insertOne(email, password);
-  return res.json({ id: insertedId, email });
-};
 
 const postNew = async (req, res) => {
   const { email, password } = req.body;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
-import { postNew } from '../controllers/UserController';
+import postNew from '../controllers/UserController';
 
 const router = Router();
 
